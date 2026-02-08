b38412885a25db57cb196ec2d918ad3d41c6e972
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-02-09 01:48:47 +1000

    Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'taski.sitkliph.com']
 
 
 # Application definition
